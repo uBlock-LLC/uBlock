50a8c03ab9326159657e2c291833e98ae96a8004
Ti Zhao <message.for.ti@gmail.com>
2015-07-09 18:05:10 -0700

    aggregated where to keep the styles for the snoozebutton

--- src/js/components/SnoozeButton.jsx
+++ src/js/components/SnoozeButton.jsx
@@ -20,7 +20,7 @@ var SnoozeButton = React.createClass({
 	onMouseLeave: function(event) {
 		this.changeHoverState(false);
 	},
-	render: function() {
+	getStyles: function(name) {
 		var goodblockData = this.props.goodblockData;
 		var bubbleBackground = 'rgba(255, 255, 255, 0.62)';
 		var bubbleBoxShadow = 'rgba(0,0,0,0.2) 3px 3px 8px';
@@ -32,72 +32,77 @@ var SnoozeButton = React.createClass({
 		}
 		var parentWidth = 70;
 		var parentHeight = 65;
-		var parentStyle = {
-			position: 'absolute',
-			width: parentWidth,
-			height: parentHeight,
-			top: (-parentHeight) * 0.6,
-			right: (-parentWidth) * 0.76,
-			zIndex: '-1',
-			cursor: 'default',
-    		fontFamily: universalStyle.fontFamily,
-    		// Radius to limit the mouse events to around
-    		// the visible area of the thought bubble.
-		    borderBottomRightRadius: '100%',
-		    borderTopLeftRadius: '90%',
-		    borderTopRightRadius: '30%',
-
-		};
-		var smallBubbleStyle = {
-			background: bubbleBackground,
-			boxShadow: bubbleBoxShadow,
-			transition: bubbleTransition,
-			position: 'absolute',
-			borderRadius: '50%',
-			width: 4,
-			height: 4,
-			left: 12,
-			bottom: 18,
-		};
-		var mediumBubbleStyle = {
-			background: bubbleBackground,
-			boxShadow: bubbleBoxShadow,
-			transition: bubbleTransition,
-			position: 'absolute',
-			borderRadius: '50%',
-			width: 12,
-			height: 12,
-			left: 15,
-			bottom: 24,
-		};
 		var bigBubbleHeight = 30;
-		var bigBubbleStyle = {
-			height: bigBubbleHeight,
-			background: bubbleBackground,
-			boxShadow: bubbleBoxShadow,
-			transition: bubbleTransition,
-			position: 'absolute',
-			borderRadius: '50%',
-			width: 40,
-			left: 24,
-			bottom: 30,
-			fontSize: 11,
-			textAlign: 'center',
-			color: textColor,
-		};
-		var bubbleTextStyle = {
-			marginTop: bigBubbleHeight * 0.3,
-		};
+		
+		return ({
+			parent: {
+				position: 'absolute',
+				width: parentWidth,
+				height: parentHeight,
+				top: (-parentHeight) * 0.6,
+				right: (-parentWidth) * 0.76,
+				zIndex: '-1',
+				cursor: 'default',
+	    		fontFamily: universalStyle.fontFamily,
+	    		// Radius to limit the mouse events to around
+	    		// the visible area of the thought bubble.
+			    borderBottomRightRadius: '100%',
+			    borderTopLeftRadius: '90%',
+			    borderTopRightRadius: '30%',
+			},
+			smallBubble: {
+				background: bubbleBackground,
+				boxShadow: bubbleBoxShadow,
+				transition: bubbleTransition,
+				position: 'absolute',
+				borderRadius: '50%',
+				width: 4,
+				height: 4,
+				left: 12,
+				bottom: 18,
+			},
+			mediumBubble: {
+				background: bubbleBackground,
+				boxShadow: bubbleBoxShadow,
+				transition: bubbleTransition,
+				position: 'absolute',
+				borderRadius: '50%',
+				width: 12,
+				height: 12,
+				left: 15,
+				bottom: 24,
+			},
+			bigBubble: {
+				height: bigBubbleHeight,
+				background: bubbleBackground,
+				boxShadow: bubbleBoxShadow,
+				transition: bubbleTransition,
+				position: 'absolute',
+				borderRadius: '50%',
+				width: 40,
+				left: 24,
+				bottom: 30,
+				fontSize: 11,
+				textAlign: 'center',
+				color: textColor,
+			},
+			bubbleText: {
+				marginTop: bigBubbleHeight * 0.3,
+			},
+		});
+	},
+	render: function() {
+		var styles = this.getStyles();
 		return (
 			<div
-				style={parentStyle}
+				style={styles.parent}
 				onClick={this.onClick}
 				onMouseEnter={this.onMouseEnter}
 				onMouseLeave={this.onMouseLeave} >
-				<div style={smallBubbleStyle} />
-				<div style={mediumBubbleStyle} />
-				<div style={bigBubbleStyle}>
-					<div style={bubbleTextStyle}>zzz...</div>
+				<div style={styles.smallBubble} />
+				<div style={styles.mediumBubble} />
+				<div style={styles.bigBubble}>
+					<div style={styles.bubbleText}>zzz...</div>
 				</div>
 			</div>
 		);
