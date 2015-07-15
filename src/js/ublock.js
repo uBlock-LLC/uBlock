afeefde6671ed053d53983bceb3bd4e9962082e5
Kevin Jennison <kevin.jennison1@gmail.com>
2015-07-14 22:12:15 -0700

    Set flag as not dev.

--- src/js/ublock.js
+++ src/js/ublock.js
@@ -405,8 +405,9 @@ var matchWhitelistDirective = function(url, hostname, directive) {
 
 // To store config info.
 µBlock.goodblock.config = {
-    // FIXME: turn off isDev.
-    isDev: true,
+    // TODO: move this into an environment variable
+    // or local storage.
+    isDev: false,
     devConfig: {
         timeMsToSnooze: 6 * 1000,
         timeMsToSleep: 20 * 1000,
