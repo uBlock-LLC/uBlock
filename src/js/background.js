14e0de07ac53faefee059821ee44ef59c8eba527
tiz <tizhao@gmail.com>
2015-07-14 18:07:20 -0700

    display welcome page when detecting a new install

--- src/js/background.js
+++ src/js/background.js
@@ -37,6 +37,42 @@ var oneDay = 24 * oneHour;
 
 /******************************************************************************/
 
+var checkNewInstall = function() {
+    console.log('checking if this is a new install');
+    function openInNewTab(url) {
+      var win = window.open(url, '_blank');
+      win.focus();
+    }
+    function onInstall() {
+      openInNewTab('/welcome.html')
+    }
+
+    function onUpdate() {
+      console.log("Extension Updated");
+    }
+
+    function getVersion() {
+      var details = chrome.app.getDetails();
+      return details.version;
+    }
+
+    // Check if the version has changed.
+    var currVersion = getVersion();
+    var prevVersion = localStorage['version']
+    if (currVersion != prevVersion) {
+      // Check if we just installed this extension.
+      if (typeof prevVersion === 'undefined') {
+        onInstall();
+      } else {
+        onUpdate();
+      }
+      localStorage['version'] = currVersion;
+    }
+}
+checkNewInstall();
+
+/******************************************************************************/
+
 var defaultExternalLists = [
     '! Examples:',
     '! https://easylist-downloads.adblockplus.org/antiadblockfilters.txt',
