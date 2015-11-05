106bc32096271723df0d097224fb6343fa5fd510
Kevin Jennison <kevin.jennison1@gmail.com>
2015-11-05 13:21:56 -0800

    Poll for token in web app when no token exists.

--- src/js/contentscript-app.js
+++ src/js/contentscript-app.js
@@ -1,8 +1,15 @@
-// Pick up the user's auth token from localstorage and provide it to
-// the extension
-var sendUserTokenIfExists = function() {
+
+var tokenPoller;
+
+// Pick up the user's auth token from localstorage.
+var getToken = function() {
   var TOKEN_LOCAL_STORAGE_KEY = 'goodblockToken';
   var token = localStorage.getItem(TOKEN_LOCAL_STORAGE_KEY);
+  return token;
+};
+
+// Send token to the extension.
+var sendUserToken = function(token) {
   if (!token) {
     console.debug('contentscript-app.js > token not found')
     return;
@@ -16,11 +23,29 @@ var sendUserTokenIfExists = function() {
     what: 'setUserAuthToken',
     token: token,
   });
-}
+
+  // Clear the poller.
+  window.clearInterval(tokenPoller);
+};
+
+// The token may be set without a page reload.
+var pollForToken = function() {
+  tokenPoller = setInterval(function() {
+    var token = getToken();
+    if (token) {
+      sendUserToken(token);
+    }
+  }, 100);
+};
 
 // Init communication with the Goodblock web app
 var initApp = function() {
-  sendUserTokenIfExists();
-}
+  var token = getToken();
+  if (token) {
+    sendUserToken(token);
+  } else {
+    pollForToken();
+  }
+};
 
 initApp();
